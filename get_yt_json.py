72d383acdf4c30313201b088f016f183826446d2
Aidar <sharipovaydar200515@gmail.com>
2024-03-12 21:31:48 +0600

    Получаю и фильтрую данные из Url

--- get_yt_json.py
+++ get_yt_json.py
@@ -1,23 +1,23 @@
 import subprocess
 import json
 import sys
-
-def get_yt_dlp_json(url):
-  try:
-    # Run yt-dlp command to get JSON data
-    result = subprocess.run(["yt-dlp", "--dump-json", url], capture_output=True, text=True)
-    if result.returncode == 0:
-        return json.loads(result.stdout)
-    else:
-        print(f"Error running yt-dlp: {result.stderr}")
-        return None
-  except Exception as e:
-    print(f"An error occurred: {e}")
-    return None
+import os
 
 def save_to_file(data, filename="yt_dlp_data.json"):
-  with open(filename, "w") as json_file:
+  directory = "jsonData"
+  file_path = os.path.join(directory, filename)
+  
+  if not os.path.exists(directory):
+    os.makedirs(directory)
+
+  with open(file_path, "w") as json_file:
     json.dump(data, json_file, indent=2)
+
+
+def get_yt_dlp_json(url):
+  command = f'yt-dlp --dump-json "{url}"'
+  output = os.popen(command).read()
+  return output
 
 if __name__ == "__main__":
   if len(sys.argv) != 2:
@@ -28,7 +28,7 @@ if __name__ == "__main__":
   yt_dlp_data = get_yt_dlp_json(yt_url)
 
   if yt_dlp_data:
-    save_to_file(yt_dlp_data)
     print("JSON data saved to yt_dlp_data.json")
+    save_to_file(yt_dlp_data)
   else:
     print("Failed to retrieve JSON data. Check the URL and make sure yt-dlp is installed.")
