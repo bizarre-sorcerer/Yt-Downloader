586d1827aa5bdd333c0f6d8619bb304f063ca0ff
Aidar <sharipovaydar200515@gmail.com>
2024-03-15 16:12:53 +0600

    Добавил пути новые для регистрации и их html начну щас

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.config['SECRET_KEY'] = 'secret-key'
 db.init_app(app)
 
+
 @app.route('/')
 def main_page():
   return render_template('index.html')
@@ -29,6 +30,39 @@ def download():
     formats=formats,
     thumbnail=thumbnail
   )
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if user.password == password:
+                login_user(user)
+                return redirect(url_for('home'))
+
+    return render_template('login.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        new_user = User(username=request.form['username'], password=request.form['password'])
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('signup.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+  
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port='43345', debug=True)
