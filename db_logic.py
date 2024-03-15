ceb47b9d5f3328a8ffdafa5bc3a3eb136e38667e
Aidar <sharipovaydar200515@gmail.com>
2024-03-16 01:25:28 +0600

    Пишу логику истории скачаных ютуб ссылок

--- db_logic.py
+++ db_logic.py
@@ -1,29 +1,11 @@
 import mysql.connector
 from flask import jsonify
 
-def connect_to_db():
-  db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="ytdownloader_data"
-  )
-  return db
-
-def add_user(username, email, password):
-  db = connect_to_db()
-  cursor = db.cursor()
-
+def add_user(cursor, username, email, password):
   add_data_query = "INSERT INTO user_data (username, email, password) VALUES (%s, %s, %s)"
   cursor.execute(add_data_query, (username, email, password))
 
-  db.commit()
-  db.close()
-
-def check_user(login, password):
-  db = connect_to_db()
-  cursor = db.cursor()
-
+def check_user(cursor, login, password):
   cursor.execute("SELECT * FROM user_data WHERE username = %s", (login,))
   user_data = cursor.fetchone()
 
@@ -38,8 +20,12 @@ def check_user(login, password):
       False,
       "Incorrect password"
     ]
-  
+      
   return [
     True,
     "Successfully signed in"
   ]
+
+def save_history(cursor, url, user_id):
+  cursor.execute("UPDATE user_data SET history = %s WHERE id = %s", (url, user_id))
+
--- server.py
+++ server.py
@@ -2,9 +2,19 @@ from flask import Flask, render_template, request, session, redirect, url_for, j
 from extractor import extractVideoData
 from filter_formats import filterFormats
 from db_logic import add_user, check_user 
+import mysql.connector
 
 app = Flask(__name__)
 app.secret_key = 'eaa2cc52a16507cf194e4f0c'
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="ytdownloader_data"
+  )
+
+cursor = db.cursor()
 
 @app.route('/')
 def main_page():
@@ -38,7 +48,7 @@ def registration():
     username = request.form["username"]
     email = request.form["email"]
     password = request.form['password']
-    add_user(username, email, password)
+    add_user(cursor, username, email, password)
 
     session['username'] = username  
     return redirect(url_for("main_page"))
@@ -52,19 +62,19 @@ def login():
     username = request.form["login"]
     password = request.form['password']   
 
-    print(check_user(username, password))
-    if check_user(username, password)[0]:
+    if check_user(cursor, username, password)[0]:
+
+      session['user_id'] = user_id
       session['username'] = username  
       return redirect(url_for("main_page"))
     else: 
-      error = check_user(username, password)[1]
+      # error = check_user(username, password)[1]
       return render_template("sign-in.html")
 
 @app.route('/log-out')
 def logout():
     session.pop('username', None)
     return redirect(url_for('main_page'))
-
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port='43345', debug=True)
